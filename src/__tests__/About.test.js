d0672dd4a134d146c0cd59db9e265dd5b5d84d19
faiza bashir <bashiirfaizona@gmail.com>
2025-04-18 12:15:11 +0300

    d

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -18,3 +18,4 @@ test("does not render a <p> element if the bio is an empty string", () => {
   const { container } = render(<About bio="" links={user.links} />);
   expect(container.querySelector("p")).toBeNull();
 });
+
